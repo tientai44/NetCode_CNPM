1f1cf120983724032bacf4f8f7e3595e07f611c8
tientai44 <97567774+tientai44@users.noreply.github.com>
2023-11-05 13:04:27 +0700

    update Character

--- Assets/_Game 2/Scripts/Manager/SpawnerController.cs
+++ Assets/_Game 2/Scripts/Manager/SpawnerController.cs
@@ -42,7 +42,7 @@ public class SpawnerController : NetworkBehaviour
         yield return new WaitForSeconds(timeDuration);
         if (!GameManager.Instance.hasServerStarted)
         {
-            yield return null;
+            yield break;
         }
         if (numMonster < 5)
         {
@@ -56,7 +56,7 @@ public class SpawnerController : NetworkBehaviour
         yield return new WaitForSeconds(timeDuration*4);
         if (!GameManager.Instance.hasServerStarted)
         {
-            yield return null;
+            yield break;
         }
         SpawnObjects();
         StartCoroutine(IESpawnBootster());
